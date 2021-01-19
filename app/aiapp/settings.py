a61e6be23619b6cdc7256359acafc8cbb738982b
jo-315 <32921100+jo-315@users.noreply.github.com>
2021-01-19 17:14:05 +0900

    tmp commit(error must be  fixed)2

--- app/aiapp/settings.py
+++ app/aiapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')+au_nes1h73nv3t9g#^+m^lj=6!bzip13h_!*(*u!_057@kct'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aiapp'
 ]
 
 MIDDLEWARE = [
