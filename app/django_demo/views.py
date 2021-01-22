dd98db25c08ceaf610bf54e67fe7dfa320e58b07
jo-315 <32921100+jo-315@users.noreply.github.com>
2021-01-23 00:55:11 +0900

    use params

--- app/django_demo/views.py
+++ app/django_demo/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, './index.html')
+  params = {
+    "welcome": "Hello, world. You're at the django DEMO index.",
+    "description": "This page shows how to use django template."
+  }
+  return render(request, './index.html', params)
